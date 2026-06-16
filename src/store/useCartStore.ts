556f960472c86d7595ce567a2b4d143fd7f326a3
savareyhano <savareyhano99@gmail.com>
2026-06-17 00:03:41 +0700

    update

--- src/store/useCartStore.ts
+++ src/store/useCartStore.ts
@@ -25,8 +25,6 @@ interface CartState {
   setTableNumber: (table: string) => void;
   totalItems: () => number;
   subtotal: () => number;
-  isCartOpen: boolean;
-  setIsCartOpen: (open: boolean) => void;
 }
 
 export const useCartStore = create<CartState>()(
@@ -36,9 +34,6 @@ export const useCartStore = create<CartState>()(
       customerName: '',
       customerPhone: '',
       tableNumber: '',
-      isCartOpen: false,
-
-      setIsCartOpen: (open) => set({ isCartOpen: open }),
 
   addItem: (item) => {
     set((state) => {
